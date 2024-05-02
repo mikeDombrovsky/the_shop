e195b991cc2c68975b9ae750827480a64226537e
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-05-02 15:10:21 +0300

    path fix gh-pages

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,22 +9,12 @@ const Header  = (props) => {
       <NavbarBrand>Blockz pizza bros</NavbarBrand>
       <Collapse navbar>
         <ul className="navbar-nav ml-auto">
-          <li
-            className={
-              props.location.pathname.endsWith("/the_shop/")  ? "nav-item active" : "nav-item"
-            }
-          >
+          <li className="nav-item active">
             <Link className="nav-link" to="/the_shop">
               Home
             </Link>
           </li>
-          <li
-            className={
-              props.location.pathname.endsWith("/the_shop/cart")
-                ? "nav-item active"
-                : "nav-item"
-            }
-          >
+          <li className="nav-item">
             <Link className="nav-link" to="/the_shop/cart">
               Cart
             </Link>
