fd88e3fe616536adab6311a7306f308b039ef71f
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-05-02 19:20:59 +0300

    path fix gh-pages

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,13 +10,13 @@ const Header  = (props) => {
       <NavbarBrand>Blockz pizza bros</NavbarBrand>
       <Collapse navbar>
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <NavLink className="nav-link" to="/the_shop" activeClassName="active">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/the_shop">
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/the_shop/cart" activeClassName="active">
+            <NavLink className="nav-link" to="/the_shop/cart">
               Cart
             </NavLink>
           </li>
