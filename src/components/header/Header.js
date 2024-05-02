8f140b62aaf70bd3f79b435413216de695272335
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-05-02 15:40:04 +0300

    path fix gh-pages

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,18 +4,19 @@ import { Link, withRouter } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header  = (props) => {
+  const [isMainActive, setIsMainActive] = React.useState(true);
   return (
     <Navbar dark expand className={styles.bg}>
       <NavbarBrand>Blockz pizza bros</NavbarBrand>
       <Collapse navbar>
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/the_shop">
+          <li className={isMainActive ? "nav-item active" : "nav-item"}>
+            <Link className="nav-link" to="/the_shop" onClick={setIsMainActive(true)}>
               Home
             </Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/the_shop/cart">
+          <li className={!isMainActive ? "nav-item active" : "nav-item"}>
+            <Link className="nav-link" to="/the_shop/cart" onClick={setIsMainActive(false)}>
               Cart
             </Link>
           </li>
